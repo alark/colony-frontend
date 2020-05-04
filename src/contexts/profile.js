7b36de0c6bcbf07f3c8d4df79b16cf04e869c720
Dustin Hsiang <dustin0116@gmail.com>
2020-05-03 21:34:24 -0700

    res test

--- src/contexts/profile.js
+++ src/contexts/profile.js
@@ -150,8 +150,8 @@ const useProfileProvider = () => {
 
   const login = credentials => axios
     .post(`${BASE_URL}/login`, credentials)
-    .catch((error) => {
-      console.log(error.response.status);
+    .catch(function(response) {
+      console.log(response.status);
     })
     .then(({ data }) => {
       dispatch({ type: LOGIN, payload: data });
